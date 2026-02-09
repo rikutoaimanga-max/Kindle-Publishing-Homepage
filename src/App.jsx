0698f8fcd3950614ce0e808596503496bb620862
rikutoaimanga-max <rikuto.aimanga@gmail.com>
2026-02-09 23:56:39 +0900

    Feat: Implement react-animated-cursor and remove CustomCursor

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,34 @@ import Reasons from './components/Reasons';
 import FAQ from './components/FAQ';
 import Footer from './components/Footer';
 import SmoothScrollWrapper from './components/SmoothScrollWrapper';
-import CustomCursor from './components/CustomCursor';
+import AnimatedCursor from 'react-animated-cursor';
 import './index.css';
 
-// App Version: CustomCursor Fix 1.1
+// App Version: CustomCursor Fix 1.2 (react-animated-cursor)
 function App() {
   return (
     <>
-      <CustomCursor />
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        color='255, 255, 255'
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={2}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
+      />
       <SmoothScrollWrapper>
         <div className="App">
           <Header />
